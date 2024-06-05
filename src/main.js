8a9c051cb79fd7e4bbf0d3542b78e3830bc8ce5b
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-06-05 13:50:07 -0700

    re-started formatting 6/5/24

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import 'bootstrap';
 const lightThemeImgSrc = 'light-mode-day-sun.svg';
 const darkThemeImgSrc = 'dark-mode-night-moon.svg';
 
+$(() => {
 // initialize the html to dark theme
 $('html').attr('data-bs-theme', 'dark');
-
+$('.hasText').attr('style', "color: #0DFF00;")
+})
 // Toggle theme
 $('#theme_toggle').on('click', () => {
   // change theme from dark mode to light mode on click
@@ -17,6 +19,7 @@ $('#theme_toggle').on('click', () => {
     $('html').attr('data-bs-theme', 'light');
     $('#theme_toggle_img').removeAttr('src');
     $('#theme_toggle_img').attr('src', darkThemeImgSrc);
+    $('.hasText').attr('style', "color: #ff7300;")
     return;
   }
   // change theme from light mode to dark mode on click
@@ -25,6 +28,7 @@ $('#theme_toggle').on('click', () => {
     $('html').attr('data-bs-theme', 'dark');
     $('#theme_toggle_img').removeAttr('src');
     $('#theme_toggle_img').attr('src', lightThemeImgSrc);
+    $('.hasText').attr('style', "color: #0DFF00;")
     return;
   }
 });
