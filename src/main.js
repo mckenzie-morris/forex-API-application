a09025b1f6b0e9bc20ba6d0cd32ff525953be665
mckenzie-morris <mckenzie.morris1111@gmail.com>
2024-06-06 14:38:45 -0700

    Progress 6/6/24

--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ const lightThemeImgSrc = 'light-mode-day-sun.svg';
 const darkThemeImgSrc = 'dark-mode-night-moon.svg';
 
 $(() => {
-// initialize the html to dark theme
-$('html').attr('data-bs-theme', 'dark');
-$('.hasText').attr('style', "color: #0DFF00;")
-})
+  // initialize the html to dark theme
+  $('html').attr('data-bs-theme', 'dark');
+  $('.hasText').attr('style', 'color: #0DFF00;');
+});
 // Toggle theme
 $('#theme_toggle').on('click', () => {
   // change theme from dark mode to light mode on click
@@ -19,7 +19,7 @@ $('#theme_toggle').on('click', () => {
     $('html').attr('data-bs-theme', 'light');
     $('#theme_toggle_img').removeAttr('src');
     $('#theme_toggle_img').attr('src', darkThemeImgSrc);
-    $('.hasText').attr('style', "color: #ff7300;")
+    $('.hasText').attr('style', 'color: #ff7300;');
     return;
   }
   // change theme from light mode to dark mode on click
@@ -28,7 +28,12 @@ $('#theme_toggle').on('click', () => {
     $('html').attr('data-bs-theme', 'dark');
     $('#theme_toggle_img').removeAttr('src');
     $('#theme_toggle_img').attr('src', lightThemeImgSrc);
-    $('.hasText').attr('style', "color: #0DFF00;")
+    $('.hasText').attr('style', 'color: #0DFF00;');
     return;
   }
 });
+
+$('li').on('click', function () {
+  const listItemElmt = $(this).data('list_item_elmt');
+  console.log(listItemElmt)
+})
